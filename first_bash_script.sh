c5cb132f135ce0aaa3700eea3e7b710fb211aa4e
palaniappan <palaniappanskm@gmail.com>
2023-09-16 00:30:41 +0530

    Hi this is palaniappan and this is my PR request

--- first_bash_script.sh
+++ first_bash_script.sh
@@ -5,10 +5,15 @@ echo "enter first number"
 read num1
 echo "enter second number"
 read num2
+echo "enter second number"
+read num3
+
 # user is entering choice
 echo "select an operation by pressing the sequence key"
 echo "1. Addition (+)"
 echo "2. Subtraction (-)"
+echo "3. MUltiplication (*)"
+
 read choice
 case $choice in
     1)
@@ -17,5 +22,9 @@ case $choice in
     2)
     result=$(($num1-$num2))
     ;;
+    3)
+    result=$(($num1*$num2))
+    ;;
 esac
 echo "The result of  is $result"
+
